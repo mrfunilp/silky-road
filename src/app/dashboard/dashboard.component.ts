39b767ca06e2d754bf222f3407619bd6cf633fed
EmilCeric <eceric11@gmail.com>
2026-09-16 23:45:10 +0200

    Added temporary item creation button dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MarketplaceItem } from '../models/marketplace-item';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [NgFor, NgIf],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+
+    items = signal<MarketplaceItem[]>([])
+
+
+    addItem() {
+        const item: MarketplaceItem = {
+            id: crypto.randomUUID(),
+            name: '',
+            biography: '',
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            creator: {
+                id: crypto.randomUUID(),
+                username: ''
+            },
+            likes: 0,
+            views: 0,
+            price: 0,
+            currency: '',
+            category: '',
+            tags: [],
+            imageUrls: [],
+            status: 'draft',
+        };
+        this.items.update((items) => [...items, item]);
+    }
+}
